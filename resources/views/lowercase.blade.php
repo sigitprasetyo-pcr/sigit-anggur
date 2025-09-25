8b7c5f39f84b57fdb7ee2a7e31d274c3e052028f
sigitprasetyo <sigitprasetyo24si@mahasiswa.pcr.ac.id>
2025-09-25 08:00:47 +0700

    Laravel awal

---
+++ resources/views/lowercase.blade.php
@@ -0,0 +1,4 @@
+index.blade.php
+auth.blade.php
+pinjam-alat.blade.php
+halaman-mahasiswa.blade.php
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/pcr', function () {
+    return 'Selamat Datang di Website Kampus PCR!';
+});
+// Route::get('/mahasiswa', function () {
+//     return 'Halo Mahasiswa';
+// });
+Route::get('/nim/{param1?}', function ($param1 = '') {
+    return 'NIM saya: '.$param1;
+});
+Route::get('/mahasiswa', function () {
+    return 'Halo Mahasiswa';
+})->name('mahasiswa.show');
+
+Route::get('/mahasiswa/{param1}',[MahasiswaController::class, 'show']);
+Route::get('/about', function () {
+    return view('halaman-about');
+});
